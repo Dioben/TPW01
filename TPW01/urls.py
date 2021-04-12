f8c938a5366c43a544c4655a675b34884a04e7f8
Diogo <diogobento@ua.pt>
2021-04-12 11:21:13 +0100

    some basics

--- TPW01/urls.py
+++ TPW01/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', views.index, name="index"),
+    path('api/latest', views.latest, name="latestGET"),
+    path('api/hot', views.popular, name='popularGET')
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from models import *
 # Create your views here.
+
+#HOME PAGE
+#TODO: put something in index.html/index.js, load latest chapters and top rated fics onto frontpage
+#  REST STUFF SHOULD BE IN DIFFERENT FILES, MAKE FILES FOR GET,POST AND DELETE POSSIBLY
+def index(request):
+    data = {}
+    return render(request, "index.html", data)
+
+
+def latest(request):
+    return Chapter.objects.reverse()[:20] #hopefully the last 20 chapters with books attached
+
+
+def popular(request):
+    return Book.objects.order_by("scoretotal/reviewcount") #unsure whether this works, just needs to be the books
+
+
+
