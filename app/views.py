d358837c9fa090a351abb932a919b6302755a112
p-laranjinha <plcasimiro@ua.pt>
2021-04-29 13:58:07 +0100

    Re:Fixed review deletion not updating score

--- app/views.py
+++ app/views.py
@@ -322,10 +322,7 @@ def deletereview(request,pk):
     review = review.get()
     if review.author != request.user and not request.user.is_staff:
         return HttpResponse("No delete permissions",403)
-    book = review.novel
-    book.scoretotal = book.scoretotal - review.rating
-    book.reviewcount -= 1
-    book.save()
+    Book.objects.filter(pk=review.novel.id).update(scoretotal=F('scoretotal') - review.rating, reviewcount=F('reviewcount') - 1)
     backurl = f'/book/{review.novel.id}/'
     review.delete()
     return redirect(backurl)
