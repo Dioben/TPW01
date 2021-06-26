4d2bedb3f7f64df33c091205bb59677b0b9ec2ac
buckaroo69 <diogobento@ua.pt>
2021-06-26 17:13:51 +0100

    fixes email problems

--- TPW01/settings.py
+++ TPW01/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.BasicAuthentication',
                                        'rest_framework.authentication.SessionAuthentication',
